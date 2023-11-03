93b611b3336fc5b21e63dafb8c561273d950b4a9
Hasdi <hryan2@uccs.edu>
2023-11-02 20:08:14 -0700

    Fixed problems and admin section of app works

--- project/django_project/settings.py
+++ project/django_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # added apps
     'project_app',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- project/project_app/views.py
+++ project/project_app/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 from django.views import generic
+from .forms import KillerForm
+from django.contrib import messages
+from django.urls import reverse
+from typing import Any
 
 # Create your views here.
+
+# generic list and detail views
+def index(request):
+   return render(request, 'project_app/index.html')
+
+class KillerListView(generic.ListView):
+   model = Killer
